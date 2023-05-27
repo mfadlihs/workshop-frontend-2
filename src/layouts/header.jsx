888f8ba4e4ee19e74f7f6639ad5c887b3b2b054d
mfadlihs <winnerfadli999@gmail.com>
2023-05-28 04:48:20 +0700

    add link in icon

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -5,7 +5,12 @@ export default function Header() {
     <div className="bg-[#E9EFF6] py-4 shadow-md px-6">
       <div className="flex justify-between max-w-4xl mx-auto">
         <div className="text-xl">Workshop Frontend 2</div>
-        <img src={GithubIcon} width={28} />
+        <a
+          href="https://github.com/mfadlihs/workshop-frontend-2"
+          target="_blank"
+        >
+          <img src={GithubIcon} width={28} />
+        </a>
       </div>
     </div>
   );
