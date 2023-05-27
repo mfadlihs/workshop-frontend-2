c3489016a86504c35662a65c14f2dc35041ddf6f
mfadlihs <winnerfadli999@gmail.com>
2023-05-28 04:41:29 +0700

    add usestate

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,47 @@ import { useState } from "react";
 import Header from "./layouts/header";
 
 function App() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    accept: false,
+  });
+
+  const onSubmit = () => {};
+
+  // e => Event Handler
+  const onChange = (e) => {
+    console.log(e);
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="h-screen">
       <Header></Header>
       <div className="px-6 h-full">
         <form className="pt-8 mx-auto w-full max-w-4xl" onSubmit={() => {}}>
           <p>Email</p>
-          <input className="mb-[10px]" />
+          <input
+            className="mb-[10px]"
+            name="email"
+            type="email"
+            value={form.email}
+            onChange={onChange}
+          />
           <p>Password</p>
-          <input className="mb-[10px]" />
+          <input
+            className="mb-[10px]"
+            name="password"
+            type="password"
+            value={form.password}
+          />
           <div className="flex space-x-1 mb-6">
-            <input type="checkbox" className="w-4 h-4" />
+            <input
+              type="checkbox"
+              className="w-4 h-4"
+              name="accept"
+              value={form.accept}
+            />
             <label>
               I agree with the{" "}
               <a className="text-blue-700 font-medium">
